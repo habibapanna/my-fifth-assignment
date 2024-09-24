ddd14e4614953b92864eea5844e47fdb19474808
habibapanna <habibapanna49@gmail.com>
2024-09-24 22:55:37 +0600

    card section done

--- js/home.js
+++ js/home.js
@@ -24,20 +24,117 @@ function toggleActive(button){
     historyBtn.style.backgroundColor = '#B4F461'
     console.log(historyBtn, donationBtn)
 }
-donationBtn.addEventListener('click', () => toggleActive(historyBtn));
+
 historyBtn.addEventListener('click', () => toggleActive(historyBtn));
+
+
 
 // blog
 
 const blogSection = document.getElementById('blog-section');
-
+const blogButton = document.getElementById('blog-btn');
+const homeButton = document.getElementById('home-btn');
 document.getElementById('blog-btn').addEventListener('click', function(){
     blogSection.style.display = 'block';
-    console.log(blogSection);
+    donationSection.style.display = 'none';
+    blogButton.style.display = 'none';
+    homeButton.style.display = 'block'
+    console.log(blogSection, donationSection, blogButton, homeButton);
+});
+document.getElementById('home-btn').addEventListener('click', function(){
+    blogSection.style.display = 'none';
+    donationSection.style.display = 'block';
+    blogButton.style.display = 'block';
+    homeButton.style.display = 'none'
+    console.log(blogSection, donationSection, blogButton, homeButton);
 });
 
 
+// donation amount 1
 
+document.getElementById('donate-now-btn')
+.addEventListener('click', function(event){
+    event.preventDefault();
+
+
+    const donateAmount = document.getElementById('donation-input-1').value;
+    const donateMoney = parseFloat(donateAmount);
+    console.log(donateAmount);
+
+    if(donateAmount >= 0){
+       const balance = document.getElementById('donation-amount').innerText;
+       const balanceNumber = parseFloat(balance);
+
+       const newBalance = balanceNumber + donateMoney;
+
+       document.getElementById('donation-amount').innerText = newBalance;
+    
+    }
+    else{
+        alert('Insufficient balance');
+            
+    }
+    
+});
+
+
+// donation amount 2
+
+document.getElementById('donate-now-btn-2')
+.addEventListener('click', function(event){
+    event.preventDefault();
+
+
+    const donateAmounts = document.getElementById('donation-input-2').value;
+    const donatesMoney = parseFloat(donateAmounts);
+    console.log(donateAmounts);
+
+    if(donateAmounts >= 0){
+       const balances = document.getElementById('donation-amount-2').innerText;
+       const balanceNumbers = parseFloat(balances);
+
+       const newBalances = balanceNumbers + donatesMoney;
+
+       document.getElementById('donation-amount-2').innerText = newBalances;
+    
+    }
+    else{
+        alert('Insufficient balance');
+            
+    }
+    
+});
+
+
+// donation amount 3
+
+document.getElementById('donate-now-btn')
+.addEventListener('click', function(event){
+    event.preventDefault();
+
+
+    const donatesAmount = document.getElementById('donation-input-3').value;
+    const donateMonies = parseFloat(donatesAmount);
+    console.log(donatesAmount);
+
+    if(donatesAmount >= 0){
+       const balanced = document.getElementById('donation-amount-3').innerText;
+       const balancedNumber = parseFloat(balanced);
+
+       const newBalanced = balancedNumber + donateMonies;
+
+       document.getElementById('donation-amount-3').innerText = newBalanced;
+    
+    }
+    else{
+        alert('Insufficient balance');
+            
+    }
+    // if(donateAmount > accountBalance){
+    //     alert('Insufficient balance');
+    //     return;
+    // }
+});
 
 // const donateNowBtns = document.querySelectorAll('.donate-now-btn');
 // let accountBalance = 500;
